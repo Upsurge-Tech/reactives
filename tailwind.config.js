35d7ab42e460f4e0a1ffed5e12d546a3bc6cfb42
mekdeskebede <kebedemekdes289@gmail.com>
2023-11-04 22:44:17 +0300

    setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#11112B",
+      },
+      fontFamily: {
+        Poppins: [
+          "sans-serif",
+          {
+            fontFeatureSettings: '"cv11", "ss01"',
+            fontVariationSettings: '"opsz" 32',
+          },
+        ],
+        "Space-Grotesk": ["sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
