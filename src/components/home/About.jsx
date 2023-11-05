65b2ca3acbb606665edeb971722bfcc1faf80b1e
fev <feventolla99@gmail.com>
2023-11-06 00:47:27 +0300

    merge

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
@@ -6,17 +6,12 @@ const About = () => {
       id="about"
       className="ml-8 mr-8 md:ml-24 md:mr-24 mt-16 sm:mt-36 md:mt-44 lg:mt-64"
     >
-
-    <div
-      id="about"
-      className="ml-8 mr-8 md:ml-24 md:mr-24 mt-16 sm:mt-36 md:mt-44 lg:mt-64"
-    >
       <h1 className="font-bold text-xl md:text-2xl pb-10">About.</h1>
       <p className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl tracking-wide">
         <span className="font-[1000] font-space leading-3">
-          We believe that{' '}
-        </span>{' '}
-        <span className="font-light italic font-sans">technology</span>{' '}
+          We believe that{" "}
+        </span>{" "}
+        <span className="font-light italic font-sans">technology</span>{" "}
       </p>
       <p className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-[1000] font-space line-clamp-1 tracking-wide">
         can transform the world.
@@ -35,9 +30,7 @@ const About = () => {
         <div className="col-span-1 md:col-span-2"></div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-
-export default About
+export default About;
