c6335cf5c25a03fd31f4e3d2c5572615c53285d3
fev <feventolla99@gmail.com>
2023-11-06 00:47:27 +0300

    merge

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -6,11 +6,6 @@ const About = () => {
       id="about"
       className="ml-8 mr-8 md:ml-24 md:mr-24 mt-16 sm:mt-36 md:mt-44 lg:mt-64"
     >
-
-    <div
-      id="about"
-      className="ml-8 mr-8 md:ml-24 md:mr-24 mt-16 sm:mt-36 md:mt-44 lg:mt-64"
-    >
       <h1 className="font-bold text-xl md:text-2xl pb-10">About.</h1>
       <p className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl tracking-wide">
         <span className="font-[1000] font-space leading-3">
