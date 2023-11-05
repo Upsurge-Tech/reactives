f3202f70b28f2a17988ada8074f142718b281458
yonatan-h <yon019hl@gmail.com>
2023-11-05 15:11:46 +0300

    finish mobile blog

--- src/pages/OurTeamAndBlog/page.jsx
+++ src/pages/OurTeamAndBlog/page.jsx
@@ -1,5 +1,4 @@
 import styles from './style.module.css'
-<<<<<<< HEAD
 import OurTeam from './OurTeam'
 import Blog from './Blog'
 
@@ -10,15 +9,6 @@ export default function OurTeamAndBlog() {
       <Blog />
     </div>
   )
-=======
 
-export default function OurTeamAndBlog (){
-
-  return (
-    <>
-    <div className={styles.red}>test1</div>
-    <div className='text-6xl'>test2</div>
-    </>
-  );
->>>>>>> ae62567 (insert page and setup styling)
 }
+
