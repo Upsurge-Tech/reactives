6c1de4e8ce62e3c3f5463e4a5eda82432a422612
fev <feventolla99@gmail.com>
2023-11-06 00:10:29 +0300

    whyus responsive

--- src/components/whyus/whyus_main.js
+++ src/components/whyus/whyus_main.js
@@ -11,11 +11,11 @@ const WhyUs = () => {
         <div className="text-white flex flex-col gap-14  items-center justify-center p-10">
           <h2 className=" font-bold text-6xl ">Why Us?</h2>
           <div
-            className="grid grid-cols-1 md:grid-cols-4 "
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"
             style={{ width: "65em" }}
           >
             <div className=" p-2 ">
-              <div className="flex flex-col">
+              <div className="flex flex-col  ">
                 <div className=" p-2 ">
                   <img
                     className="pb-5"
@@ -97,6 +97,48 @@ const WhyUs = () => {
         </div>
       </div>
     </main>
+
+    //     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+    //   <div className="p-4">
+    //     <div className="flex flex-col items-center text-center">
+    //       <img src={art_1} alt="art 1" width={70} height={70} className="pb-2" />
+    //       <div className="font-semibold">Expertise</div>
+    //       <div className="text-sm">
+    //         Our team of experienced experts have the knowledge and expertise to deliver innovative IT solutions that meet your unique needs.
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   <div className="p-4">
+    //     <div className="flex flex-col items-center text-center">
+    //       <img src={art_2} alt="art 2" width={70} height={70} className="pb-2" />
+    //       <div className="font-semibold">Technology</div>
+    //       <div className="text-sm">
+    //         We stay up to date with the latest trends and technologies in the IT industry, so you can get the most advanced solutions available.
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   <div className="p-4">
+    //     <div className="flex flex-col items-center text-center">
+    //       <img src={art_3} alt="art 3" width={70} height={70} className="pb-2" />
+    //       <div className="font-semibold">Solutions</div>
+    //       <div className="text-sm">
+    //         We take a personalized approach to every project, working closely with you to understand your business and create solutions.
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   <div className="p-4">
+    //     <div className="flex flex-col items-center text-center">
+    //       <img src={art_4} alt="art 4" width={70} height={70} className="pb-2" />
+    //       <div className="font-semibold">Results</div>
+    //       <div className="text-sm">
+    //         Our track record speaks for itself. We've helped businesses of all sizes and industries achieve their goals with our IT solutions.
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
