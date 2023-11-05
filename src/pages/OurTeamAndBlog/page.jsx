403e4683c2a40d0be54f4894cf2663d2460e38a4
yonatan-h <yon019hl@gmail.com>
2023-11-05 16:53:43 +0300

    card hover effect added

--- src/pages/OurTeamAndBlog/page.jsx
+++ src/pages/OurTeamAndBlog/page.jsx
@@ -1,5 +1,7 @@
 import styles from './style.module.css'
 import jim from './jim.png'
+import { useState } from 'react'
+import whiteArrow from './assets/white-arrow.svg'
 
 export default function OurTeamAndBlog() {
   return (
@@ -33,7 +35,7 @@ function OurTeam() {
           The passionate people behind our every projects in WaveNet.
         </p>
 
-        <div className={'pt-[120px] grid gap-5 grid-cols-2 grid-rows-[300px]'}>
+        <div className={'pt-[120px] grid gap-5 grid-cols-2'}>
           {people.map(TeamCard)}
         </div>
       </section>
@@ -42,12 +44,35 @@ function OurTeam() {
 }
 
 function TeamCard({ title, name, link, img }) {
+  const [hover, setHover] = useState(false)
+  let imageClass =
+    'rounded-[20px] absolute w-full object-cover ' + styles['transition']
+  if (hover) imageClass += ' h-full '
+  else imageClass += ' h-[70%] '
+
+  let overlayClass = `${styles['overlay']} ${styles['transition']} absolute w-full h-full bg-black `
+  if (hover) overlayClass += styles['overlay-visible']
+
+  let arrowClass =  'h-[16px] '
+  if (!hover) arrowClass += 'hidden'
+
   return (
-    <div className="flex flex-col gap-2" key={name + link}>
-      <img src={img} alt={name} className="rounded-[25px]" />
-      <div>
-        <p className="text-[]">{title}</p>
-        <p className="font-[600]">{name}</p>
+    <div
+      className="relative h-[300px] overflow-hidden rounded-[20px]"
+      key={name}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <img src={img} alt={name} className={imageClass} />
+
+      <div className={overlayClass}></div>
+
+      <div className="absolute bottom-2 left-0 right-0 px-5">
+        <p className="">{title}</p>
+        <div className="flex justify-between items-end">
+          <p className="font-[600]">{name}</p>
+          <img src={whiteArrow} alt="link arrow" className={arrowClass} />
+        </div>
       </div>
     </div>
   )
@@ -89,7 +114,7 @@ function Blog() {
         styles['dark-blue-text-children']
       }
     >
-      <section className='flex gap-10 flex-col'>
+      <section className="flex gap-10 flex-col">
         <div className="flex flex-col gap-5">
           <h2 className="p-2 text-6xl font-[700] font-spaceGrotesk">Blog</h2>
           <button className="rounded-[15px] border-[0.5px] border-dark-blue px-[40px]">
@@ -97,8 +122,8 @@ function Blog() {
           </button>
         </div>
 
-        <div className='flex flex-col items-center'>
-          <div className='flex gap-[40px]'>
+        <div className="flex flex-col items-center">
+          <div className="flex gap-[40px]">
             <button>left</button>
             <button>right</button>
           </div>
@@ -114,10 +139,12 @@ function Blog() {
 
 function BlogBox({ category, content, number }) {
   return (
-    <div className='border-t border-dark-blue'>
-      <p className='pt-[56px]'>{category.toUpperCase()}</p>
+    <div className="border-t border-dark-blue">
+      <p className="pt-[56px]">{category.toUpperCase()}</p>
       <p className="font-[600] md:text-[32px] pt-[24px]">{content}</p>
-      <p className='font-spaceGrotesk font-[700] text-4xl md:text-[50px] pt-[80px]'>{number}</p>
+      <p className="font-spaceGrotesk font-[700] text-4xl md:text-[50px] pt-[80px]">
+        {number}
+      </p>
     </div>
   )
 }
