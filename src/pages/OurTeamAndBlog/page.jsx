b323cbdef2061e537de407188ad43006ffc1c7b1
yonatan-h <yon019hl@gmail.com>
2023-11-05 12:34:48 +0300

    copy all text from figma

--- src/pages/OurTeamAndBlog/page.jsx
+++ src/pages/OurTeamAndBlog/page.jsx
@@ -1,11 +1,69 @@
 import styles from './style.module.css'
 
-export default function OurTeamAndBlog (){
-
+export default function OurTeamAndBlog() {
   return (
     <>
-    <div className={styles.red}>test1</div>
-    <div className='text-6xl'>test2</div>
+      <OurTeam />
+      <Blog />
     </>
-  );
+  )
+}
+
+function OurTeam() {
+  const people = [
+    { title: 'CEO & Founder', name: 'Robert Kang' },
+    { title: 'Project Manager', name: 'Emily Brown' },
+    { title: 'Finance Manager', name: 'David Chung' },
+
+    { title: 'HR Manager', name: 'Sarah Lee' },
+    { title: 'Head Engineer', name: 'Michael John' },
+    { title: 'Head Analyst', name: 'Jenefer Kim' },
+  ]
+
+  return (
+    <div>
+      <section>
+        <h2 className="">Our Team</h2>
+        <p>The passionate people behind our every projects in WaveNet.</p>
+      </section>
+    </div>
+  )
+}
+
+function Blog() {
+  const blogs = [
+    {
+      category: 'tech',
+      content:
+        'Why Cloud Computing is Essential for Your Business in Year 2023',
+      number: '01.03',
+    },
+    {
+      category: 'review',
+      content:
+        '5 Ways Software Developer Can Boost Your Business Quicker Than Ever',
+      number: '20.05',
+    },
+    {
+      category: 'business',
+      content:
+        '5 Ways Business Developer Can Boost Your Business Quicker Than Ever',
+      number: '20.05',
+    },
+    {
+      category: 'family',
+      content:
+        '10 Children Business Developers that Can Boost Your Business Quicker Than Ever',
+      number: '20.05',
+    },
+  ]
+
+  return (
+    <div>
+      <section>
+        <h2 className="">Blog</h2>
+        <button>Read More Artilces</button>
+      </section>
+    </div>
+  )
 }
