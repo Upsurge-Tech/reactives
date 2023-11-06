76db7c87ac768144bfd5cc5a07d5c6c32282a4ef
yonatan-h <99685585+yonatan-h@users.noreply.github.com>
2023-11-06 18:28:21 +0300

    Feature/our team and blog page without testing (#13)
    * insert page and setup styling
    
    * copy all text from figma
    
    * add basic coloring
    
    * finish our-team mobile
    
    * finish mobile blog
    
    * card hover effect added
    
    * finish desktop teams section
    
    * finish desktop blog section
    
    * smooth card hover
    
    * make blog arrows functional
    
    * give components thier own files
    
    * swap jim`s profile picture with blank white picture
    
    * Feature/home page (#9)
    
    * setup tailwind
    
    * implement NavBar
    
    * add home page
    
    * fix build error
    
    * fix build error
    
    * fix build error
    
    * adding test
    
    * finish merge 2
    
    * merge again
    
    * why us setup (#3)
    
    * why us setup
    
    * inserting page
    
    * component based
    
    * asset organization
    
    * layouts
    
    * layouts
    
    * whyus responsive
    
    * test
    
    * merge
    
    * resolve merge
    
    * insert page and setup styling
    
    * finish mobile blog
    
    * Feature/home page (#9)
    
    * setup tailwind
    
    * implement NavBar
    
    * add home page
    
    * fix build error
    
    * fix build error
    
    * fix build error
    
    * adding test
    
    * finish merge 2
    
    * link navbar to blog section
    
    * fix merge leftover errors
    
    * comment test
    
    ---------
    
    Co-authored-by: Mekdes Kebede <91591156+Mekdeskebede@users.noreply.github.com>
    Co-authored-by: Feven Tolla <88964869+Feventolla@users.noreply.github.com>

---
+++ src/pages/OurTeamAndBlog/Blog.jsx
@@ -0,0 +1,92 @@
+import styles from './style.module.css'
+import { useState } from 'react'
+import longArrow from './assets/long-arrow.svg'
+import rightArrow from './assets/right-arrow.svg'
+import leftArrow from './assets/left-arrow.svg'
+
+import BlogBox from './BlogBox'
+
+export default function Blog() {
+  const [index, setIndex] = useState(0)
+  const blogs = [
+    {
+      category: 'tech',
+      content:
+        'Why Cloud Computing is Essential for Your Business in Year 2023',
+      number: '01.03',
+    },
+    {
+      category: 'review',
+      content:
+        '5 Ways Software Developer Can Boost Your Business Quicker Than Ever',
+      number: '20.05',
+    },
+    {
+      category: 'business',
+      content:
+        '5 Ways Business Developer Can Boost Your Business Quicker Than Ever',
+      number: '20.05',
+    },
+    {
+      category: 'family',
+      content:
+        '10 Children Business Developers that Can Boost Your Business Quicker Than Ever',
+      number: '20.05',
+    },
+    {
+      category: 'hidden',
+      content:
+        '10 Children Business Developers that Can Boost Your Business Quicker Than Ever',
+      number: '20.05',
+    },
+  ]
+
+  const hasLeft = index > 0
+  const hasRight = index < blogs.length - 2
+  //go right or left if possible
+  const goRight = () => hasRight && setIndex(index + 2)
+  const goLeft = () => hasLeft && setIndex(index - 2)
+
+  return (
+    <div
+      id="blog"
+      className={
+        styles['container'] +
+        ' bg-light-blue ' +
+        styles['dark-blue-text-children']
+      }
+    >
+      <section className="max-w-[1620px] m-auto">
+        <div className="flex gap-10 flex-col md:flex-row justify-between ">
+          <div className="flex flex-col gap-5 justify-between">
+            <h2 className="text-6xl md:text-[100px] font-[700] font-spaceGrotesk">
+              Blog
+            </h2>
+            <button className="rounded-[15px] text-[16px] border-[0.5px] border-dark-blue px-[40px] py-[20px] flex gap-[18px]">
+              READ MORE ARTICLES <img src={longArrow} alt="arrow" />
+            </button>
+          </div>
+
+          <div className="flex flex-col">
+            <div className="flex gap-[40px] h-[150px] justify-end">
+              <button className={hasLeft ? '' : 'opacity-50'} onClick={goLeft}>
+                <img src={leftArrow} alt="left" />
+              </button>
+              <button
+                className={hasRight ? '' : 'opacity-50'}
+                onClick={goRight}
+              >
+                <img src={rightArrow} alt="right" />
+              </button>
+            </div>
+
+            <div className="flex gap-[30px]">
+              {blogs[index] && BlogBox(blogs[index])}
+              {blogs[index + 1] && BlogBox(blogs[index + 1])}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
---
+++ src/pages/OurTeamAndBlog/page.jsx
@@ -0,0 +1,14 @@
+import styles from './style.module.css'
+import OurTeam from './OurTeam'
+import Blog from './Blog'
+
+export default function OurTeamAndBlog() {
+  return (
+    <div className={styles['fs-20-children']}>
+      <OurTeam />
+      <Blog />
+    </div>
+  )
+
+}
+
