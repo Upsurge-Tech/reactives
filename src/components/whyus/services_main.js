821f95296d549d22a12d2705b7297ed79af7fc8b
fev <feventolla99@gmail.com>
2023-11-06 22:38:15 +0300

    modification

--- src/components/whyus/services_main.js
+++ src/components/whyus/services_main.js
@@ -5,12 +5,12 @@ import arrow2 from "../../assets/next.png";
 const ServicesMain = () => {
   return (
     <main id="service">
-      <div className="bg-blue-50 min-w-screen  flex justify-center items-center pt-20  ">
+      <div className="bg-blue-50 min-w-screen  flex justify-center items-center pt-20 pb-20  ">
         <div className="md:flex flex-wrap">
-          <div className="md:w-1/2 p-4">
-            <div className="relative bg-slate-50 text-2xl rounded-3xl shadow mb-4 w-56 h-48 flex flex-col p-7 space-x-4 gap-5 group hover:bg-[#11112B] hover:text-white">
+          <div className="flex flex-col justify-center items-center  lg:pl-20 pb-3">
+            <div className="relative bg-slate-50 text-2xl rounded-3xl shadow mb-4 w-80 md:w-56 h-48 flex flex-col  p-7 space-x-4 gap-5 group hover:bg-[#11112B] hover:text-white  ">
               <img
-                className="opacity-0 group-hover:opacity-100"
+                className="opacity-0 group-hover:opacity-100 "
                 alt="arrow"
                 src={arrow}
                 width={25}
@@ -20,63 +20,52 @@ const ServicesMain = () => {
                 Web <br /> Development
               </p>
             </div>
-          </div>
-          <div className="md:w-1/2 p-4">
-            <div className="relative bg-slate-50 text-2xl rounded-3xl shadow mb-4 w-56 h-48 flex flex-col p-7 space-x-4 gap-5 group hover:bg-[#11112B] hover:text-white">
+            <div className="relative bg-slate-50 text-2xl  rounded-3xl shadow mb-4 w-80 md:w-56 h-48 flex flex-col  p-7 space-x-4 gap-5 group hover:bg-[#11112B] hover:text-white  ">
               <img
-                className="opacity-0 group-hover:opacity-100"
+                className="opacity-0 group-hover:opacity-100 "
                 alt="arrow"
                 src={arrow}
                 width={25}
                 height={25}
               />
-              <p>
-                Mobile <br /> Development
-              </p>
+              Mobile <br /> Development
             </div>
-          </div>
-          <div className="md:w-1/2 p-4">
-            <div className="relative bg-slate-50 text-2xl rounded-3xl shadow w-56 h-48 flex flex-col p-7 space-x-4 gap-5 group hover:bg-[#11112B] hover:text-white">
+            <div className="relative bg-slate-50 text-2xl  rounded-3xl shadow w-80 md:w-56 h-48 flex flex-col  p-7 space-x-4 gap-5 group hover:bg-[#11112B] hover:text-white  ">
               <img
-                className="opacity-0 group-hover:opacity-100"
+                className="opacity-0 group-hover:opacity-100 "
                 alt="arrow"
                 src={arrow}
                 width={25}
                 height={25}
               />
-              <p>
-                Cyber <br /> Security
-              </p>
+              Cyber <br /> Security
             </div>
           </div>
-          <div className="md:w-1/2 p-4">
-            <div className="relative bg-slate-50 text-2xl rounded-3xl shadow mb-4 w-56 h-48 flex flex-col p-7 space-x-4 gap-5 group hover:bg-[#11112B] hover:text-white">
+          <div className="  flex flex-col justify-center items-center  lg:pl-20">
+            <div className="relative bg-slate-50 text-2xl  rounded-3xl shadow mb-4 w-80 md:w-56 h-48 flex flex-col  p-7 space-x-4 gap-5 group hover:bg-[#11112B] hover:text-white  ">
               <img
-                className="opacity-0 group-hover:opacity-100"
+                className="opacity-0 group-hover:opacity-100 "
                 alt="arrow"
                 src={arrow}
                 width={25}
                 height={25}
               />
-              <p>
-                Cloud <br /> Computing
-              </p>
+              Cloud <br />
+              Computing
             </div>
-          </div>
-          <div className="md:w-1/2 p-4">
-            <div className="relative bg-slate-50 text-2xl rounded-3xl shadow w-56 h-48 flex flex-col p-7 space-x-4 gap-5 group hover:bg-[#11112B] hover:text-white">
+            <div className="relative bg-slate-50 text-2xl rounded-3xl shadow w-80 md:w-56 h-48 flex flex-col  p-7 space-x-4 gap-5 group hover:bg-[#11112B] hover:text-white  ">
               <img
-                className="opacity-0 group-hover:opacity-100"
+                className="opacity-0 group-hover:opacity-100 "
                 alt="arrow"
                 src={arrow}
                 width={25}
                 height={25}
               />
-              <p>Digital marketing</p>
+              Digital marketing
             </div>
           </div>
-          <div className="md:w-1/2 p-4">
-            <div className="flex flex-col w-64 h-56 gap-5">
+          <div className="flex flex-col justify-center items-center pl-20 p-10  ">
+            <div className=" flex flex-col w-64 h-56 gap-5">
               <h2 className="text-6xl font-bold">Services</h2>
               <p className="text-sm pb-5">
                 We provide customized solutions that meet our clients' unique
