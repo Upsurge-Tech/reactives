9e4b78f75c742909bb41fd029d78d20a9873c045
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2023-11-06 11:01:19 +0300

    Projects (#8)
    * Adding new project section features
    
    * updating index.js file
    
    * updating files
    
    * adding packages
    
    * updating files
    
    * updating files
    
    * update test

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import HomePage from "./pages/home";
 // import logo from "./logo.svg";
 import WhyusService from "./pages/Whyus_Services";
 // import BlueSection from "./pages/whyus/whyus_main";
+import Projects from "./pages/Projects";
+import Sucess from "./pages/Sucess";
+import './App.css'
 
 function App() {
   return (
     <main>
       <HomePage />
-
       <WhyusService />
+      <div id="zoom">
+        <Projects />
+        <Sucess />
+      </div>
     </main>
-    // comment
   );
 }
 
