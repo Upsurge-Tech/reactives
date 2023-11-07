d6d0aa1189a4dd03dc4bf88a9afd0079acf9894d
fev <feventolla99@gmail.com>
2023-11-07 10:09:56 +0300

    resolve

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import "./App.css";
 
 import HomePage from "./pages/home";
 import OurTeamAndBlog from "./pages/OurTeamAndBlog/page";
 import WhyusService from "./pages/Whyus_Services";
-import HomePage from "./pages/home";
-import OurTeamAndBlog from "./pages/OurTeamAndBlog/page";
-import WhyusService from "./pages/Whyus_Services";
+
 // import BlueSection from "./pages/whyus/whyus_main";
-import Projects from "./pages/Projects";
-import Sucess from "./pages/Sucess";
-import "./App.css";
 import Projects from "./pages/Projects";
 import Sucess from "./pages/Sucess";
 import "./App.css";
@@ -32,5 +26,4 @@ function App() {
   );
 }
 
-export default App;
 export default App;
